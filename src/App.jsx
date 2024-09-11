dbfb5a198710aaa6c1e7c776e7e01ad871f67a94
Pritam Roy <pritamroy1136@gmail.com>
2024-09-11 17:47:19 +0530

    Making table better with header

--- src/App.jsx
+++ src/App.jsx
@@ -103,17 +103,6 @@ function App() {
               </tbody>
             </table>
           </div>
-
-          <div className="mt-6 text-center">
-            <a
-              href={
-                "https://hackernoon.com/lang/es/como-usar-google-sheets-api-con-nodejs-cz3v316f"
-              }
-              className="inline-block rounded-md bg-indigo-600 px-4 py-2 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Download Salary Slip
-            </a>
-          </div>
         </div>
       </div>
     </div>
