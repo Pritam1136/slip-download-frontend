e97e42ceaeafe2eeeb8a478abf480dc56468632a
Pritam Roy <pritamroy1136@gmail.com>
2024-09-17 14:59:44 +0530

    Corrected the URL

--- src/URL.js
+++ src/URL.js
@@ -1,4 +1,4 @@
-export const url = "http://localhost:5000";
+export const url = "https://slip-download-backend.onrender.com";
 export const imageSrc = `https://media.licdn.com/dms/image/v2/C4D0BAQH6fJz1s57_eA/company-logo_200_200/company-logo_200_200/0/1630509348990/forwardcode_techstudio_logo?e=1734566400&v=beta&t=SYIHhlwQbjb19D52xG4NoSZLEy5a7tUSUYPWmZMjNkk`;
 
 // https://slip-download-backend.onrender.com
